67cbd8a054e4c4ddb76a57dd7ad15c4adae41080
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-24 14:10:28 +0100

    feature: login function created and tested

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -2,9 +2,9 @@ const User = require("./model");
 
 //Refactor: check if user already exists before creating new user?
 const registerUser = async (req, res) => {
-  console.log("req.body", req.body)
+  console.log("req.body", req.body);
   try {
-   const user = await User.create(req.body);
+    const user = await User.create(req.body);
 
     res.status(201).json({ message: "success", user: user });
   } catch (error) {
@@ -12,21 +12,44 @@ const registerUser = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  
+  try {
+    const user =await User.findOne({where: {username}})
+
+    if (!user) {
+      return res.status(404).json({ message: "user not found"})
+    }
+
+    const userInfo = {
+      id: user.id,
+      username: user.username,
+    }
+
+    res.status(200).json({ message: "success", user: userInfo })
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+}
+
+ 
 const removeAllUsers = async (req, res) => {
-  console.log("req.body", req.body)
+  console.log("req.body", req.body);
   try {
     await User.destroy({
       where: {},
       truncate: true,
-    })
+    });
 
-    res.status(200).json({ message: "success"})
+    res.status(200).json({ message: "success" });
   } catch (error) {
-    res.staus(500).json
+    res.staus(500).json;
   }
-}
+};
 
 module.exports = {
   registerUser,
+  login,
   removeAllUsers,
 };
