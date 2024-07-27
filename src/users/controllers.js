425292b25a3e14b93b5a491ceabb96fb26d68dd9
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-27 17:13:18 +0100

    last: last save before push

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,6 +1,6 @@
 const User = require("./model");
-const jwt = require("jsonwebtoken")
-const SECRET_KEY = process.env.SECRET
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.SECRET;
 
 //Refactor: check if user already exists before creating new user?
 const registerUser = async (req, res) => {
@@ -16,29 +16,28 @@ const registerUser = async (req, res) => {
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  
+
   try {
-    const user =await User.findOne({where: {username}})
+    const user = await User.findOne({ where: { username } });
 
     if (!user) {
-      return res.status(404).json({ message: "user not found"})
+      return res.status(404).json({ message: "user not found" });
     }
 
-    const token = jwt.sign({ id: user.id}, SECRET_KEY, {expiresIn: '3h'})
+    const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: "3h" });
 
     const userInfo = {
       id: user.id,
       username: user.username,
       token: token,
-    }
+    };
 
-    res.status(200).json({ message: "success", user: userInfo })
+    res.status(200).json({ message: "success", user: userInfo });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
-}
+};
 
- 
 const removeAllUsers = async (req, res) => {
   console.log("req.body", req.body);
   try {
@@ -55,13 +54,13 @@ const removeAllUsers = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   try {
-    const user = await User.findAll()
+    const user = await User.findAll();
 
-    res.status(200).json({ message: "success", user: user})
+    res.status(200).json({ message: "success", user: user });
   } catch (error) {
-    res.status(501).json({ message: error.message, error: error })
+    res.status(501).json({ message: error.message, error: error });
   }
-}
+};
 
 module.exports = {
   registerUser,
